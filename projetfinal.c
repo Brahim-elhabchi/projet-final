ca7d2cf7e6d60bb99a692b13ec053350225df582
brahim-elhabchi <bhabachi227@gmail.com>
2025-09-09 10:20:59 +0100

    second fonction in final project

--- projetfinal.c
+++ projetfinal.c
@@ -39,3 +39,50 @@
     }
     return 0 ;
 }
+int afficherplayers()
+    
+{
+    if(nbrjouours==0)
+    {
+        printf("il n y a pas de joueurs dans l equipe \n");
+        return 0;
+    }
+    for ( int i=0 ; i<nbrjouours ; i++)
+    {
+        printf("le joueur %d : \n",i+1);
+        printf("nom : %s \n",equipe[i].name);
+        printf("age : %d \n",equipe[i].age);
+        printf("nombre de buts : %d \n",equipe[i].goals);
+        printf("numero de maillot : %d \n",equipe[i].numero_maillot);
+        printf("poste : %s \n",equipe[i].poste);
+        printf("id : %d \n",equipe[i].id);
+        printf("\n");
+    }
+}
+int rechercherjoueur()
+{
+    if(nbrjouours==0)
+    {
+        printf("il n y a pas de joueurs dans l equipe \n");
+        return 0;
+    }
+    int id ;
+    printf("donner l'id du joueur a rechercher : ");
+    scanf("%d",&id);
+    for ( int i=0 ; i<nbrjouours ; i++)
+    {
+        if(equipe[i].id==id)
+        {
+            printf("le joueur %s a ete trouve avec succes \n",equipe[i].name);
+            printf("nom : %s \n",equipe[i].name);
+            printf("age : %d \n",equipe[i].age);
+            printf("nombre de buts : %d \n",equipe[i].goals);
+            printf("numero de maillot : %d \n",equipe[i].numero_maillot);
+            printf("poste : %s \n",equipe[i].poste);
+            printf("id : %d \n",equipe[i].id);
+            return 0;
+        }
+    }
+    printf("le joueur avec l'id %d n'a pas ete trouve \n",id);
+    return 0;
+}
